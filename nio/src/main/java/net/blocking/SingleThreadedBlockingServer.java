051cc08481e300c0d3595b4472694051e712e4db
Patrick Haun <patrick.haun@epost-dev.de>
2014-08-22 14:44:30 +0200

    Added single threaded blocking server

---
+++ nio/src/main/java/net/blocking/SingleThreadedBlockingServer.java
@@ -0,0 +1,37 @@
+package net.blocking;
+
+import net.util.Magic;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SingleThreadedBlockingServer {
+
+
+    public static void main(String[] args) throws IOException {
+
+        final ServerSocket serverSocket = new ServerSocket(9999);
+
+        while (true) {
+
+            final Socket socket = serverSocket.accept();
+            try (
+                    Socket s = socket;
+                    InputStream is = socket.getInputStream();
+                    OutputStream os = socket.getOutputStream()
+            ) {
+
+                for (int i = 0; (i = is.read()) != -1; ) {
+                    os.write(Magic.doMagic(i));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
