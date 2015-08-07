c646ee336b48a08f2933f9b7b96aecd891126799
Patrick Haun <patrick.haun@immobilienscout24.de>
2015-08-07 15:21:56 +0200

    akka stream server

---
+++ nio/src/main/scala/net/akka/AkkaStreamServer.scala
@@ -0,0 +1,39 @@
+package net.akka
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializerSettings, ActorMaterializer}
+import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
+import akka.stream.scaladsl.{Flow, Source, Tcp}
+import akka.util.ByteString
+import net.util.Magic
+
+import scala.concurrent.Future
+
+object AkkaStreamServer extends App {
+
+  implicit val actorSystem = ActorSystem("stream-echo-service-system")
+
+  implicit val materializer = ActorMaterializer(
+    ActorMaterializerSettings(actorSystem)
+      .withInputBuffer(
+        initialSize = 64,
+        maxSize = 64)
+  )
+
+  val connections: Source[IncomingConnection, Future[ServerBinding]] =
+    Tcp().bind("127.0.0.1", 9999)
+
+  connections runForeach { connection =>
+    println(s"New connection from: ${connection.remoteAddress}")
+
+    val magicFlow = Flow[ByteString]
+      .map(b => b.toByteBuffer.array())
+      .map(_.map(b => Magic.doMagic(b.toInt).toByte))
+      .map(ByteString.apply)
+
+    connection.handleWith(magicFlow)
+  }
+
+}
+
+
